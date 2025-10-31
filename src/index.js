24c65a364475efc2e25cff18b6fd23d635f23d0d
SF Knute <s.fedotov@knute.edu.ua>
2025-10-31 09:00:20 +0200

    added .env file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import express from 'express'
 import { menu } from './menu.js';
 import { orders } from './orders.js';
+
+import dotenv from 'dotenv'
+dotenv.config();
+
 import { API_KEY } from './auth.js';
 
 let server = express();
 let port = 8080;
+
+let API_KEY2= process.env.API_KEY2;
+
+
 
 server.get('/', (request, response) => {
     response.send('I am working');
@@ -19,6 +27,16 @@ server.get('/menu', (request, response) => {
 server.get('/orders', (request, response) => {
     let key = request.header('authorization');
     if (key == API_KEY) {
+        response.json(orders);
+    } else {
+        response.status(401).send("wrong API key")
+    }
+});
+
+server.get('/orders2', (request, response) => {
+    console.log(`API_KEY2 = ${API_KEY2}`);
+    let key = request.header('authorization');
+    if (key == API_KEY2) {
         response.json(orders);
     } else {
         response.status(401).send("wrong API key")
